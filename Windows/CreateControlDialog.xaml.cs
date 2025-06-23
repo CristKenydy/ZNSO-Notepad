d5b903aaadc2e122716c8d8c0f2942be5932610d
CristKenydy <znsocorporation@outlook.com>
2025-06-23 10:52:07 +0800

    Add files via upload

---
+++ Windows/CreateControlDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZNSO_Notepad.Windows
+{
+    /// <summary>
+    /// Interaction logic for CreateControlDialog.xaml
+    /// </summary>
+    public partial class CreateControlDialog : Window
+    {
+        public CreateControlDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void newTEbutton_click(object sender, RoutedEventArgs e)
+        {
+            App.znpEditor.BtnAddTextEdit();
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void newIBbutton_click(object sender, RoutedEventArgs e)
+        {
+            App.znpEditor.BtnAddIVBar();
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
---
+++ Windows/OptionsDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ZNSO_Notepad.Windows
+{
+    public partial class OptionsDialog : Window
+    {
+        public OptionsDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyTheme_Click(object sender, RoutedEventArgs e)
+        {
+            App.ChangeTheme(ThemeComboBox.SelectedItem.ToString());
+        }
+    }
+}
